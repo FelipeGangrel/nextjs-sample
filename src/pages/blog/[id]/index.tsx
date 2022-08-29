4efae7eccdf49fe1ac24f74541396247a4b309d2
Felipe Gangrel <felipefrog@gmail.com>
2022-08-29 14:58:32 -0300

    Exploring parallel promises

--- src/pages/blog/[id]/index.tsx
+++ src/pages/blog/[id]/index.tsx
@@ -11,17 +11,37 @@ interface BlogPost {
   content: string;
 }
 
+interface Product {
+  id: number;
+  name: string;
+}
+
 interface PageProps {
   post: BlogPost;
+  products: Product[];
 }
 
 interface QueryParams extends ParsedUrlQuery {
   id: string;
 }
 
+const getProductsData = async (): Promise<Product[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, name: "Product 1" },
+        { id: 2, name: "Product 2" },
+        { id: 3, name: "Product 3" },
+        { id: 4, name: "Product 4" },
+        { id: 5, name: "Product 5" },
+      ]);
+    }, 2_000);
+  });
+};
+
 const getBlogPost = async (id: string): Promise<BlogPost> => {
   return new Promise((resolve, reject) => {
-    const willResolve = Math.random() > 0.5;
+    const willResolve = Math.random() > 0.2;
 
     if (!willResolve) reject("Error fetching blog post");
 
@@ -51,17 +71,29 @@ export const getStaticProps: GetStaticProps<PageProps, QueryParams> = async (
 ) => {
   // using non-null assertion operator because we know that the id param is defined
   const { id } = context.params!;
-  const post = await getBlogPost(id);
+
+  console.time("getting data in parallel");
+  const [post, products] = await Promise.all([
+    getBlogPost(id),
+    getProductsData(),
+  ]);
+  console.timeEnd("getting data in parallel");
+
+  console.time("getting data in sequence");
+  const post2 = await getBlogPost(id);
+  const products2 = await getProductsData();
+  console.timeEnd("getting data in sequence");
 
   return {
     props: {
       post,
+      products,
       ...(await serverSideTranslations(context.locale!, ["blog-page"])),
     },
   };
 };
 
-const BlogPostPage: NextPage<PageProps> = ({ post }) => {
+const BlogPostPage: NextPage<PageProps> = ({ post, products }) => {
   const router = useRouter();
   const { t } = useTranslation("blog-page");
   const { id, message } = router.query;
@@ -73,6 +105,7 @@ const BlogPostPage: NextPage<PageProps> = ({ post }) => {
         <p>{t("no_message")}</p>
       )}
       <pre>{JSON.stringify(post, null, 2)}</pre>
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </S.PageContainer>
   );
 };
