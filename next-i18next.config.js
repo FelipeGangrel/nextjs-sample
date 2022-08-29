b16b94b4b3f8105c3febb34cd1d0f4e2c4d461cb
Felipe Gangrel <felipefrog@gmail.com>
2022-08-29 10:23:02 -0300

    Updating i17next configs

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   debug: process.env.NODE_ENV !== "production",
   reloadOnPrerender: process.env.NODE_ENV !== "production",
+  nonExplicitSupportedLngs: true,
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt-BR", "pt-PT"],
   },
   fallbackLng: {
     default: ["en"],
-    "pt-PT": ["pt-BR"], // pt-PT will fallback to pt-BR
+    "pt-PT": ["pt-BR", "en"], // if a key is not found in pt-PT, try pt-BR and then en
   },
 };
